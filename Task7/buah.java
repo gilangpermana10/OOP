23715b4f9b9f9ab4e6a31e8fc3545e831ddd5ab2
gilangpermana10 <gilanghyper27@gmail.com>
2021-11-16 14:21:22 +0700

    Initial Commit

---
+++ Task7/buah.java
@@ -0,0 +1,17 @@
+public class buah extends hasilTani{
+
+
+    public static void main(String[] args) {
+    }
+
+}
+
+class hasilTani{
+    int jumlah;
+    String namaBuah[] = new String[jumlah];
+
+    hasilTani(String namaBuah,int jumlah){
+        this.jumlah = jumlah;
+        namaBuah = namaBuah[];
+    }
+}
